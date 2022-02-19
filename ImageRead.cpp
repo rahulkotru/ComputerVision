b99a4382b26a92e431dc1e28afd7c319a1734106
Rahul <rahulkotr1@gmail.com>
2022-02-19 22:47:56 +0530

    Resolved deltas

--- ImageRead.cpp
+++ ImageRead.cpp
@@ -1,13 +1,10 @@
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 #include<iostream>
 
 using namespace std;
-using namespace cv;
+
 
 int main(){
-    string path="";
+   
     cout<<"Hello World";
     return 0;
     
