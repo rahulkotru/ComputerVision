89d67b517a09b923cd8880fa30047c1d4efc5a23
Rahul <rahulkotr1@gmail.com>
2022-02-20 17:04:18 +0530

    Video and Webcam feed

--- ImageRead.cpp
+++ ImageRead.cpp
@@ -8,7 +8,26 @@ using namespace std;
 void main()
 {
 string path="Resources/Test.png";
+string path2="Resources/p.mp4";
 Mat img=imread(path);
 imshow("Image",img);
-waitKey(0);
+waitKey(10);
+cout<<"Displaying Video Capture"<<endl;
+VideoCapture vid(path2);
+Mat image;
+/*while(true){
+    vid.read(image);
+    imshow("Video",image);
+    waitKey(10);
+}
+*/
+cout<<"Webcam video feed"<<endl;
+VideoCapture webcam(0);
+Mat cam;
+while(true){
+    webcam.read(cam);
+    imshow("Webcam Feed",cam);
+    waitKey(1);
+}
+
 }
