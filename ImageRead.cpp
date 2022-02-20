225354a3e406988ce926ead593728ee750fe8764
Rahul <rahulkotr1@gmail.com>
2022-02-20 16:19:35 +0530

    Configure OpenCV

--- ImageRead.cpp
+++ ImageRead.cpp
@@ -1,21 +1,19 @@
-#include<iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
-using namespace std;
-class comp{
-    private:
-    int number;
-    public:
-    string name;
-    comp(){
-        
+using namespace cv;
+int main(int argc, char** argv )
+{
+
+    Mat image;
+    image = imread("D:/28_GitHub/ComputerVision/opencvtest/Resources/Test.png");
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
     }
-
-};
-
-
-int main(){
-   
-    cout<<"Hello World";
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
     return 0;
-    
 }
