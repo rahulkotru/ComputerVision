17d1a0cb353840e7afe146123194001685f6f585
Rahul <rahulkotr1@gmail.com>
2022-02-20 19:20:32 +0530

    Build Success

--- OneReadingImageData/ImageRead.cpp
+++ OneReadingImageData/ImageRead.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main()
 {
 string path="D:/28_GitHub/ComputerVision/OneReadingImageData/Resources/Test.png";
-//string path2="Resources/p.mp4";
 Mat img=imread(path);
 imshow("Image",img);
-waitKey(0);
-//cout<<"Displaying Video Capture"<<endl;
-//VideoCapture vid(path2);
-//Mat image;
-/*while(true){
+waitKey(500);
+cout<<"Displaying Video Capture"<<endl;
+string path2="D:/28_GitHub/ComputerVision/OneReadingImageData/Resources/p.mp4";
+VideoCapture vid(path2);
+Mat image;
+while(true){
     vid.read(image);
     imshow("Video",image);
     waitKey(10);
@@ -29,6 +29,6 @@ while(true){
     imshow("Webcam Feed",cam);
     waitKey(1);
 }
-*/
+
 return 0;
 }
