35d166d90d93201e564e665d867b51557228786b
Rahul <rahulkotr1@gmail.com>
2022-02-20 19:15:48 +0530

    Build Success

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/28_GitHub/ComputerVision
-# Build directory: D:/28_GitHub/ComputerVision/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
