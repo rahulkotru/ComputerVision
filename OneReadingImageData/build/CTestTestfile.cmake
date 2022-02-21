7df5c0181d5672809ba189581100ab4bdb14d0c8
Rahul <rahulkotr1@gmail.com>
2022-02-21 20:21:06 +0530

    Image processing

--- OneReadingImageData/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/28_GitHub/ComputerVision/OneReadingImageData
-# Build directory: D:/28_GitHub/ComputerVision/OneReadingImageData/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
