3cdf5b20ff9871dd1b8847e67742deca3de04695
Rahul <rahulkotr1@gmail.com>
2022-02-21 15:47:46 +0530

    Basic image processing

--- OneReadingImageData/ImageRead.cpp
+++ OneReadingImageData/ImageRead.cpp
@@ -10,17 +10,17 @@ int main()
 string path="D:/28_GitHub/ComputerVision/OneReadingImageData/Resources/Test.png";
 Mat img=imread(path);
 imshow("Image",img);
-waitKey(500);
+waitKey(250);
 cout<<"Displaying Video Capture"<<endl;
 string path2="D:/28_GitHub/ComputerVision/OneReadingImageData/Resources/p.mp4";
 VideoCapture vid(path2);
 Mat image;
-while(true){
+/*while(true){
     vid.read(image);
     imshow("Video",image);
     waitKey(10);
 }
-
+*/
 cout<<"Webcam video feed"<<endl;
 VideoCapture webcam(0);
 Mat cam;
@@ -28,6 +28,7 @@ while(true){
     webcam.read(cam);
     imshow("Webcam Feed",cam);
     waitKey(1);
+    destroyAllWindows();
 }
 
 return 0;
