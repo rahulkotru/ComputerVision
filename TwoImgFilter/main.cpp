8d3fbe35c65dad94d3f45a2f090e6f41eb4bf04e
Rahul <rahulkotr1@gmail.com>
2022-05-23 19:14:18 +0530

    Working Image read

--- TwoImgFilter/main.cpp
+++ TwoImgFilter/main.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
+#include<iostream>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+using namespace cv;
+
+int main(){
+    string path="D:/28_GitHub/ComputerVision/TwoImgFilter/asset/test.jpg";
+    Mat img=imread(path);
+    Mat imgGray;
+    cvtColor(img,imgGray,COLOR_BGR2GRAY);
+    resize(img,img,Size(480,640));
+    //resizeWindow("Test Image",300,400);
+    namedWindow("Test Image",WINDOW_AUTOSIZE);
+    imshow("Test Image",img);
+    //
+    waitKey(); 
+    /*imshow("Imgage",imgGray);*/
+    /*namedWindow("Test Image");
+    waitKey(1000); 
+    Mat destImg;
+    GaussianBlur(img,imgGray,Size(7,7),5,0);
+    namedWindow("Gaussian Test Image");
+    //imshow("Gaussian",imgGray);
+    waitKey(1000);
+
+    Canny(imgGray,img,25,75);
+    namedWindow("Canny Test Image");
+    //imshow("Gaussian",img);
+    waitKey();*/
+    /*destroyAllWindows();*/
+    return 0;
 }
