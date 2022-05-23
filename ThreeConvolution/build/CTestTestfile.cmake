5a94375158966ddab6fa7879505de4feb0a52734
Rahul <rahulkotr1@gmail.com>
2022-05-23 23:24:16 +0530

    Convolutions

---
+++ ThreeConvolution/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/28_GitHub/ComputerVision/ThreeConvolution
+# Build directory: D:/28_GitHub/ComputerVision/ThreeConvolution/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ThreeConvolution/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+    int main(){
+        
+    }
