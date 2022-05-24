6c52da382d82cda5c456bacc4731102778e7ecff
Rahul <rahulkotr1@gmail.com>
2022-05-24 20:26:47 +0530

    Modyfing code

--- ThreeConvolution/main.cpp
+++ ThreeConvolution/main.cpp
@@ -7,5 +7,13 @@ using namespace std;
 using namespace cv;
     int main(){
         Mat img=imread("D:/28_GitHub/ComputerVision/TwoImgFilter/asset/test.jpg");
-        
+        if (img.empty()){
+            cout<<"Empty file"<<endl;
+        }
+        Mat kernelOne=(Mat_<double>(3,3)<< 0,0,0,0,1,0,0,0,0);
+        Mat identity;
+        filter2D(img,identity,-1,kernelOne,Point(-1,-1),0,4);
+        resizeWindow("Convolutional Kernel",Size(480,640));
+        imshow("Convolutional Kernel",img);
+        waitKey();
     }
