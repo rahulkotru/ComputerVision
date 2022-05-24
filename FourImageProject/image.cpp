a66016b571e4f597f4c437fa2aa50341c12c4354
Rahul <rahulkotr1@gmail.com>
2022-05-24 21:01:31 +0530

    Create local header file

--- FourImageProject/image.cpp
+++ FourImageProject/image.cpp
@@ -2,10 +2,10 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <string>
+
 
 class image{
-    auto readImage(string address){
-
+    auto readImage(std::string address){
+        
     }
 };
